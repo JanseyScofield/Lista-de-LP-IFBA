b095457766cef0a2a89b7eadd7e864a2c6342320
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-03 23:49:34 -0300

    Questão 49.c

--- Questão 49.c
+++ Questão 49.c
@@ -1,1 +1,1 @@
-É apenas a questão 48 duplicada. Acesse esse link para checkar a resolução: https://github.com/JanseyScofield/Lista-de-LP-IFBA/blob/main/Quest%C3%A3o%2048
+É apenas a questão 48 duplicada. Acesse esse link para checkar a resolução: https://github.com/JanseyScofield/Lista-de-LP-IFBA/blob/d74a78e707fff954842289a00fa4befb2be9e3aa/Quest%C3%A3o%2048.c
