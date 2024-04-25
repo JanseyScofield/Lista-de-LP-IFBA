0c3dcc70011e5dfa21854caf74d1029c5e448100
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-24 22:46:04 -0300

    Questão 72.c

---
+++ Questão 72.c
@@ -0,0 +1,37 @@
+/* Escreva um programa que leia um vetor de 10 posições ordenados de inteiros e um inteiro. O programa deve informar a primeira posição onde este inteiro ocorre no 
+vetor ou -1 caso o valor não ocorra no vetor (Busca Binária).*/
+
+#include <stdio.h>
+
+main(){
+	int vet[10], iCont, meio,inicio = 0, fim = 9, num;
+	int jCont;
+	
+	for(iCont = 0; iCont < 10; iCont++)
+		scanf("%d",  &vet[iCont]);
+	printf("Digite um valor para buscar: ");
+	scanf("%d", &num);
+	jCont = 0;
+	
+	while(inicio <= fim){
+		meio = (inicio + fim)/2;
+		
+		if(vet[meio] == num){
+			jCont = 1;
+			break;
+		}
+		else if(vet[meio] > num){
+			fim = meio - 1;
+		}
+		else {
+			inicio = meio +1;
+		}
+	}
+	
+	if(jCont == 1){
+		printf("Ocorre no vetor!");
+	}
+	else{
+		printf("-1");
+	}
+}
