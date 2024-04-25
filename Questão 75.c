edf087ed42685314d41fe704deafb163778fa247
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-24 23:22:28 -0300

    Questão 75.c

---
+++ Questão 75.c
@@ -0,0 +1,25 @@
+/*Escreva um programa que ordene um array de inteiros de 15 posições utilizando o método da bolha (bubble sort).*/
+
+#include <stdio.h>
+
+main(){
+	int vet[15], aux, iCont, jCont;
+	
+	for(iCont = 0; iCont < 15; iCont++){
+		scanf("%d", &vet[iCont]);
+	}
+	
+	for(iCont = 0; iCont < 15; iCont++){
+		for(jCont = 0; jCont < 14; jCont++){
+			if(vet[jCont] > vet[jCont+1]){
+				aux = vet[jCont];
+				vet[jCont] = vet[jCont+1];
+				vet[jCont+1] = aux;
+			}
+		}
+	}
+	
+	for(iCont = 0; iCont < 15;iCont++){
+		printf("%d\n", vet[iCont]);
+	}
+}
